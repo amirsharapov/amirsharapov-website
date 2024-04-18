244c5f40ab5602a23d7994d7d8efe0dfc64438c6
Amir Sharapov <amirsharapov.personal@gmail.com>
2024-04-17 21:30:47 -0500

    wording

--- src/content/about.js
+++ src/content/about.js
@@ -3,12 +3,12 @@ export const ABOUT_PAGE_CONTENT = `\
 
 Hello! My name is Amir 👋 and I have a keen interest in automation and ML
 
-I started my software engineering journey in 2020, starting with basic HTML / CSS / JS.
+I started my journey in 2020, starting with basic HTML / CSS / JS.
 Since then, I've been immersed in creating everything from web apps 💻 to ML-powered gaming bots 🎮🧠.
 
 (Game bots were and still are my favorite projects!)
 
-Currently, I work as a software engineer at PIMCO and
+Currently, I'm as a software engineer at PIMCO and
 am pursuing a BS in Computer Science at [WGU](https://wgu.edu).
 Alongside my studies, I'm tackling a few machine learning projects to sharpen my skills and
 explore new technologies.
