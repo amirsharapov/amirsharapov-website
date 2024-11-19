c0388b6bdf906cf587d1810ebe5ce8b6b17cab43
Amir Sharapov <amirsharapov.personal@gmail.com>
2024-11-19 02:27:28 -0600

    removed front page

--- src/content/home.js
+++ src/content/home.js
@@ -1,18 +1,5 @@
 export const HOME_PAGE_CONTENT = `\
-# Hi, I'm Amir 👋
+## Amir Sharapov
 
-I'm a SWE @ PIMCO with an interest in ML and anything automation.
-
-If you're here, you might be interested to check out a little [about me](/about) 👨‍💻,
-or check out the [latest projects](/projects) I'm working on 🤖🧠.
-
-If those don't scratch your itch, no worries - here are other page(s) you can explore:
-
-- [ML Resources](/ml-resources)
-
-...and last but not least, if you're interested in expanding your network, you can connect 💬 with me on these platforms:
-
-- [LinkedIn](https://www.linkedin.com/in/amirsharapov/)
-- [Discord](https://discord.gg/Aq6pN8fUTt)
-- [GitHub](https://github.com/amirsharapov)
+### Software Engineer (2020 - Present)
 `;
